f5c31f25a8c5ecc487e28d5e62bdd1be89a854ec
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2026-04-12 00:03:29 +0530

    Intro to JavaScript, Data Types, Variables, Constants, Displaying Output, Popups, Operators

---
+++ JavaScript/index.js
@@ -0,0 +1,50 @@
+// console.log("Andrea")
+// console.log(5)
+
+// var studentName = "Andrea"
+// var rollNo = 10
+// var hasPet = false
+// var carBrand = null
+
+// console.log(studentName)
+// console.log(rollNo)
+// console.log(hasPet)
+// console.log(carBrand)
+
+// hasPet = true
+
+// console.log(studentName)
+// console.log(rollNo)
+// console.log(hasPet)
+
+/* -------------------- */
+// studentName = 'Jane'
+// console.log(studentName)
+// alert(studentName)
+// document.write(studentName)
+// document.getElementById('test').innerHTML = studentName
+
+// document.getElementById('div-1').innerHTML = "<h1>Hi everyone</h1><p>We are going to learn about JavaScript!!!</p>"
+
+/* -------------------- */
+// alert('You have logged in successfully')
+// var result = confirm("Are you sure you want to logout?")
+// console.log(result)
+var result = prompt("Enter your name")
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+/* -------------------- */
+// SINGLE LINE COMMENT
+/* 
+  MULTI LINE COMMENT
+*/
